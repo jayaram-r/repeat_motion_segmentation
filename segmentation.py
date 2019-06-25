16f2de90f94d6d3dd41c234639626fe3e30abfe4
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2019-06-25 17:55:08 -0500

    - Number of segments does not need to be specified now. - Can have templates from multiple actions.

--- segmentation.py
+++ segmentation.py
@@ -16,10 +16,11 @@ import os
 import numpy as np
 import copy
 import operator
+import time
 import multiprocessing
 from functools import partial
 import dtw
-from tslearn.metrics import dtw_path as dtw_tslearn
+# from tslearn.metrics import dtw_path as dtw_tslearn
 import logging
 import matplotlib
 matplotlib.use('Agg')
@@ -80,37 +81,38 @@ def generate_sequence(n, curve='sine', noise=True, noise_level=0.005):
             xf = noise_level * np.random.rand(nf)
             x = np.concatenate([x, xf])
 
-    return x
+    return x[:, np.newaxis]
 
 
 def average_distance_to_templates(sequence, templates):
-    val = 0.0
-    for seq in templates:
-        d, _, _, _ = dtw.accelerated_dtw(sequence[:, np.newaxis], seq[:, np.newaxis], dist='cityblock')
-        val += d
+    val_min = np.inf
+    label = 1
+    for i, t in enumerate(templates, start=1):
+        val = 0.0
+        for seq in t:
+            d, _, _, _ = dtw.accelerated_dtw(sequence, seq, dist='cityblock')
+            val += d
 
-    return val / len(templates)
+        val /= len(t)
+        if val < val_min:
+            val_min = val
+            label = i
+
+    return val_min, label
 
 
-def helper_search(sequence, templates, reverse_search, m):
-    if reverse_search:
-        seq = sequence[-m:]
-    else:
-        seq = sequence[:m]
-
-    return m, average_distance_to_templates(seq, templates)
+def helper_search(sequence, templates, m):
+    ret = average_distance_to_templates(sequence[:m, :], templates)
+    return m, ret[0], ret[1]
 
 
-def search_subsequence(sequence, templates, avg_length=None, reverse_search=False):
+def search_subsequence(sequence, templates, avg_length=None):
     """
     Search for the subsequence that leads to minimum average DTW distance to the template sequences.
-    The search can be from the right (reverse) by setting `reverse_search = True`.
-    Specify `avg_length` to restrict the search to the range `[0.5 * avg_length, 2 * avg_length]`.
 
     :param sequence: numpy array of shape (N, ) with float values.
     :param templates: list of numpy arrays, where each array is a template or reference sequence.
     :param avg_length: None or an int value.
-    :param reverse_search: True or False
 
     :return: tuple (a, b), where `a` is the best subsequence length and `b` is the minimum average distance.
     """
@@ -124,7 +126,7 @@ def search_subsequence(sequence, templates, avg_length=None, reverse_search=Fals
 
     num_proc = max(1, multiprocessing.cpu_count() - 1)
     if num_proc > 1:
-        helper_search_partial = partial(helper_search, sequence, templates, reverse_search)
+        helper_search_partial = partial(helper_search, sequence, templates)
         pool_obj = multiprocessing.Pool(processes=num_proc)
         results = []
         _ = pool_obj.map_async(helper_search_partial, range(l_min, l_max + 1), chunksize=None,
@@ -133,69 +135,49 @@ def search_subsequence(sequence, templates, avg_length=None, reverse_search=Fals
         pool_obj.join()
     else:
         # num_proc = 1
-        results = [helper_search(sequence, templates, reverse_search, m) for m in range(l_min, l_max + 1)]
+        results = [helper_search(sequence, templates, m) for m in range(l_min, l_max + 1)]
 
     # Return the subsequence length that leads to minimum average DTW distance to the template sequences
     return min(results, key=operator.itemgetter(1))
 
 
-def segment_repeat_sequences(data, templates, num_segments):
+def segment_repeat_sequences(data, templates):
     """
+    Segment the sequence `data` to closely match the sequences specified in the list `templates`.
 
-    :param data: numpy array of shape (N, ) with float values.
-    :param templates: list of numpy arrays, where each array is a template or reference sequence.
-    :param num_segments: integer number of segments required. Should be >= 1.
-
+    :param data: numpy array of shape (N, 1) with float values corresponding to the data sequence.
+    :param templates: list `[L_1, . . ., L_k]`, where each `L_i` is another list `L_i = [s_i1, . . ., s_im]`, and
+                      each `s_ij` is a numpy array (of shape (M, 1)) corresponding to a template sequence.
     :return:
-        list of numpy arrays (of length `num_segments`) which are the segmented subsequences.
+        list of segmented subsequences, each of which are numpy arrays of shape (m, 1).
     """
-    if num_segments < 1:
-        raise ValueError("Invalid value '{}' for the input 'num_segments'.".format(num_segments))
+    # Average length of templates and minimum length required to be considered for segmentation
+    avg_length = int(np.round(np.mean([b.shape[0] for a in templates for b in a])))
+    min_length = max(2, int(np.floor(0.5 * avg_length)))
 
-    if num_segments == 1:
-        logger.warning("Input 'num_segments' is 1. Returning input sequence without segmentation.")
-        return [copy.deepcopy(data)]
-
-    logger.info("Length of input sequence = %d. Number of segments required = %d", data.shape[0], num_segments)
-    if num_segments == 2:
-        m, d1 = search_subsequence(data, templates)
-        d2 = average_distance_to_templates(data[m:], templates)
-        data_segments = [data[:m], data[m:]]
-        logger.info("Length of subsequence 1 = %d. Average DTW distance to the templates = %.6f", m, d1)
-        logger.info("Length of subsequence 2 = %d. Average DTW distance to the templates = %.6f",
-                    data.shape[0] - m, d2)
-    else:
-        # Starting from the left end of the sequence, find the subsequence with minimum average DTW distance from
-        # the templates. Repeat this iteratively to find the segments
-        data_segments = []
-        data_rem = copy.copy(data)
-        num_segments_rem = num_segments
-        k = 1
-        while num_segments_rem > 1:
-            avg_length = int(np.floor(data_rem.shape[0] / float(num_segments_rem)))
-            m, d1 = search_subsequence(data_rem, templates, avg_length=avg_length)
-            data_segments.append(data_rem[:m])
-            data_rem = data_rem[m:]
-            logger.info("Length of subsequence %d = %d. Average DTW distance to the templates = %.6f", k, m, d1)
-            num_segments_rem -= 1
-            k += 1
-
-        data_segments.append(data_rem)
-        d1 = average_distance_to_templates(data_rem, templates)
-        logger.info("Length of subsequence %d = %d. Average DTW distance to the templates = %.6f",
-                    k, data_rem.shape[0], d1)
+    # Starting from the left end of the sequence, find the subsequence with minimum average DTW distance from
+    # the templates. Repeat this iteratively to find the segments
+    data_segments = []
+    data_rem = copy.copy(data)
+    k = 1
+    while data_rem.shape[0] >= min_length:
+        m, d_min, label = search_subsequence(data_rem, templates, avg_length=avg_length)
+        data_segments.append(data_rem[:m, :])
+        data_rem = data_rem[m:, :]
+        logger.info("Length of subsequence %d = %d. Matched template label = %d. Average DTW distance to the "
+                    "matched templates = %.6f.", k, m, label, d_min)
+        k += 1
 
     return data_segments
 
 
 def main():
-    np.random.seed(seed=12345)
+    np.random.seed(seed=157)
     # Choose one of: 'sine', 'cosine', 'gaussian', 'gaussian_inverted'
     curve = 'sine'
 
-    # Parameters for data generation
     # Number of repetitions (segments) in the sequence
-    num_repeat = 10
+    num_repeat = 15
 
     # The length of each repetition subsequence is picked at random from this interval
     length_range = [100, 125]
@@ -208,31 +190,28 @@ def main():
             generate_sequence(np.random.randint(length_range[0], high=length_range[1]), curve=curve)
         )
 
-    # Generate and plot the data sequence
+    template_sequences = [template_sequences]
+
+    # Generate the data sequence
     data_sequence = []
     for _ in range(num_repeat):
         data_sequence.append(
             generate_sequence(np.random.randint(length_range[0], high=length_range[1]), curve=curve)
         )
 
-    # Calculate the average DTW distance between each repetition subsequence and the template sequences.
-    avg_dist = np.zeros(num_repeat)
-    for i, seq1 in enumerate(data_sequence):
-        avg_dist[i] = average_distance_to_templates(seq1, template_sequences)
-        logger.info("Average DTW distance between subsequence %d and the template sequences = %.6f",
-                    i + 1, avg_dist[i])
-
-    logger.info("")
     # Concatenate the repetition subsequences into one. The segmentation algorithm takes this sequence as input.
-    data_sequence_concat = np.concatenate(data_sequence)
+    data_sequence = np.concatenate(data_sequence)
 
+    t1 = time.time()
     # Perform segmentation of the concateneted sequence
-    data_segments = segment_repeat_sequences(data_sequence_concat, template_sequences, num_repeat)
+    data_segments = segment_repeat_sequences(data_sequence, template_sequences)
+    t2 = time.time()
+    logger.info("Time taken for segmentation = %.2f seconds", t2 - t1)
 
     # Plot the concatenated sequence and the result of the segmentation
     fig = plt.figure()
     ax1 = fig.add_subplot(2, 1, 1)
-    ax1.plot(np.arange(data_sequence_concat.shape[0]), data_sequence_concat, linestyle='--', color='r',
+    ax1.plot(np.arange(data_sequence.shape[0]), data_sequence[:, 0], linestyle='--', color='r',
              marker='.', markersize=4)
     # ax1.set_xlabel("index", fontsize=10, fontweight='bold')
     ax1.set_ylabel("value", fontsize=10, fontweight='bold')
@@ -243,7 +222,8 @@ def main():
     nc = len(COLORS_LIST)
     for j, seg in enumerate(data_segments):
         en = st + seg.shape[0] - 1
-        ax1.plot(np.arange(st, en + 1), seg, linestyle='--', color=COLORS_LIST[j % nc], marker='.', markersize=4)
+        ax1.plot(np.arange(st, en + 1), seg[:, 0], linestyle='--', color=COLORS_LIST[j % nc],
+                 marker='.', markersize=4)
         st = en + 1
 
     ax1.set_xlabel("index", fontsize=10, fontweight='bold')
